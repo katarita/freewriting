9a19fe1e5c14805f7ec5ec4fe1316edec44f1f65
Kate Kholyavkina <katerina.kholyavkina@gmail.com>
2017-12-30 06:17:22 +0200

    initial commit. first working draft, notes are saving to the local storage

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import AddNote from './components/AddNote';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: JSON.parse(localStorage.getItem("notes"))
+    }
+  }
+  // componentDidMount() {
+  //   let savedNotes = JSON.parse(localStorage.getItem("notes"));
+  //   console.log('savedNotes', savedNotes);
+  //   this.setState = {
+  //     notes: savedNotes
+  //   }
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('nextProps, nextState', nextProps, nextState);
+    localStorage.setItem("notes", JSON.stringify(nextState.notes));
+  }
+
+
+  addNote (note, symbols, words) {
+    let date = new Date();
+    let id = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+
+    this.setState({
+      notes: {
+        ...this.state.notes,
+        [id]: {
+          note: note,
+          symbols: symbols,
+          words: words
+        }
+      }
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <div className="container">
+          <h1>Clear your mind. Write down your thoughts.</h1>
+          <AddNote notes={this.state.notes} addNote={this.addNote.bind(this)} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/AddNote.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class AddNote extends Component {
+
+
+  changeHandler (e) {
+    let s = e.target.value;
+    s = s.replace(/(^\s*)|(\s*$)/gi,"");
+	  s = s.replace(/[ ]{2,}/gi," ");
+	  s = s.replace(/\n /,"\n");
+    this.props.addNote(e.target.value, e.target.value.length, s.split(' ').length);
+  }
+
+  render () {
+    let date = new Date();
+    let id = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    let todayNote = this.props.notes && this.props.notes[id] ? this.props.notes[id] : { note: '', symbols: 0, words: 0};
+
+    console.log('this.props.notes', this.props.notes);
+
+    return (
+      <div>
+        <textarea value={todayNote.note} onChange={this.changeHandler.bind(this)} />
+        {/* <div>Letters count: {todayNote.symbols}</div> */}
+        <div>{todayNote.words} of 500 words</div>
+      </div>
+    )
+  }
+}
+
+export default AddNote;
